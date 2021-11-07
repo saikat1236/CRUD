a9859163e7e8ab6b357b47a34f3cc72b3fd808aa
Saikat Biswas <saikat1236@gmail.com>
2021-11-07 17:53:57 +0530

    updated

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,7 @@
 const axios=require('axios');
 
 exports.homeRoutes = (req,res)=>{
-    axios.get('http://localhost:3000/api/users')
+    axios.get('https://crud-website1.herokuapp.com/api/users')
     .then(function(response){
         res.render('index',{users: response.data});
     })
@@ -14,7 +14,7 @@ exports.add_user= (req,res)=>{
     res.render('add_user');
 }
 exports.update_user=(req,res)=>{
-    axios.get('http://localhost:3000/api/users',{params:{id: req.query.id}})
+    axios.get('https://crud-website1.herokuapp.com/api/users',{params:{id: req.query.id}})
     .then(function(userdata){
         res.render('update_user',{user: userdata.data})
     })
