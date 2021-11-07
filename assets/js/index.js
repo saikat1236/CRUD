f229991353338d84e4f978556644682a30feee0c
Saikat Biswas <saikat1236@gmail.com>
2021-11-07 17:57:45 +0530

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,7 +40,7 @@ $("#update_user").submit(function(event){
 
 
     var request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `https://crud-website1.herokuapp.com/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -57,7 +57,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `https://crud-website1.herokuapp.com/api/users/${id}`,
             "method" : "DELETE"
         }
 
